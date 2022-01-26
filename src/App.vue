7741c131ba530f1769eee698c27615f9eabc38f7
xg <444712055@qq.com>
2022-01-26 15:15:30 +0800

    feat: 封装热词云图组件-随机运动

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+      <WordCloudMove :words="words"></WordCloudMove>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import WordCloudMove from '@/components/WordCloudMove.vue'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    WordCloudMove
+  },
+  data() {
+    return {
+      words: ['万事如意', '事事如意 ', '万事亨通', '一帆风顺', '万事大吉', '吉祥如意', '步步高升', '步步登高', '三羊开泰', '得心应手', '财源广进', '陶未媲美', '阖家安康', '龙马精神', '锦绣前程', '吉祥如意', '生龙活虎', '神采奕奕', '五谷丰登', '马到成功', '飞黄腾达', ' 步步高升', '福禄寿禧']
+    }
   }
 }
 </script>
